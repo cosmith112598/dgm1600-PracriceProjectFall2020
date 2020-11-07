6dc4539e310630975bbeb7d5f6e5f4701b7f5a1e
Courtnee Smith <courtneesmith1995@gmail.com>
2020-11-06 18:53:10 -0700

    files and links

---
+++ mandalorian/starships/main.js
@@ -0,0 +1,50 @@
+import { starships } from '../data/starships.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.main')
+
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+function populateNav(starships) {
+    starships.forEach(starship => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', event => {
+            let shipName = event.target.textContent
+            const foundShip = starships.find(ship => ship.name === shipName)
+            populateShipView(foundShip)
+        })
+
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+        nav.appendChild(navList)
+    })
+}
+
+function populateShipView(shipData) {
+    removeChildren(shipView)
+    // use createElement to make new img elements
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    // set their src to the url below
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    // make shipNum act like charNum from the characters page
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        dialog.classList.toggle("is-active")
+    }) // genius level 965!! Yes we checked!!
+    
+    shipView.appendChild(shipImage)
+}
+
+populateNav(starships)  
